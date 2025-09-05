e03ab1af199cff6c30805ffa750d588e072ef0b0
DanielVelasquez0511 <166188331+DanielVelasquez0511@users.noreply.github.com>
2025-09-05 06:56:29 -0500

    avance

--- analisis/src/DANIEL/Exercise.java
+++ analisis/src/DANIEL/Exercise.java
@@ -18,6 +18,7 @@ public class Exercise {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.print("Enter number of elements: ");
         int n = sc.nextInt();
 
